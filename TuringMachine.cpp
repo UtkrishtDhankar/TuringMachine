943e954c32cddae12c18cba5b2882d92f0e09c78
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-05-03 16:33:35 +0530

    Started adding some memory things

--- TuringMachine.cpp
+++ TuringMachine.cpp
@@ -2,21 +2,29 @@
 
 TuringMachine::TuringMachine()
 {
-
+	InitializeMemory();
 }
 
 TuringMachine::TuringMachine(std::vector<Card> cards_in) : 
 	cards(cards_in)
 {
-
+	InitializeMemory();
 }
 
 TuringMachine::TuringMachine(const TuringMachine& other)
 {
+	InitializeMemory();
 	cards = other.cards;
 }
 
 TuringMachine::TuringMachine(TuringMachine&& other)
 {
+	InitializeMemory();
 	cards = std::move(other.cards);
+}
+
+void TuringMachine::InitializeMemory()
+{
+	memory.push_back(false);
+	currentPos = memory.begin();
 }
--- TuringMachine.hpp
+++ TuringMachine.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <list>
 #include <utility>
 
 #include "Card.hpp"
@@ -9,11 +10,19 @@
 class TuringMachine 
 {
 private:
-	std::vector<Card>		cards;
+	std::vector<Card>			cards;
+	std::list<bool>				memory;
+
+	std::list<bool>::iterator	currentPos;
+
+	/*
+	 * Initializes the memory of the TuringMachine.
+	 */
+	void						InitializeMemory();
 
 public:
-							TuringMachine();
-							TuringMachine(std::vector<Card> cards);
-							TuringMachine(const TuringMachine& other);
-							TuringMachine(TuringMachine&& other);
+								TuringMachine();
+								TuringMachine(std::vector<Card> cards);
+								TuringMachine(const TuringMachine& other);
+								TuringMachine(TuringMachine&& other);
 };
