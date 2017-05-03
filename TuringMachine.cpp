3c9e905a09cf72ad419c7e11a96959c44857c525
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-05-03 17:06:08 +0530

    Implemented TuringMachine mostly I think

--- TuringMachine.cpp
+++ TuringMachine.cpp
@@ -1,20 +1,63 @@
 #include "TuringMachine.hpp"
 
+/*
+ * Private Functions
+ */
+void TuringMachine::InitializeMemory()
+{
+	memory.push_back(false);
+	currentPos = memory.begin();
+}
+
+void TuringMachine::ExecuteInstruction(const Instruction& i)
+{
+	*currentPos = i.overwrite;
+
+	auto oldPos = currentPos;
+	if (i.moveType == MoveType::LEFT) 
+	{
+		// If we don't have the memory, create it
+		if (currentPos == memory.begin()) {
+			memory.push_front(false);
+		}
+
+		currentPos--;
+	}
+	else if (i.moveType == MoveType::RIGHT)
+	{
+		// If we don't have the memory, create it
+		if (currentPos == --memory.end())
+		{
+			memory.push_front(false);
+		}
+
+		currentPos++;
+	}
+
+	currentCard = i.nextCard;
+}
+
+/*
+ * Constructors
+ */
 TuringMachine::TuringMachine()
 {
 	InitializeMemory();
+	currentCard = 0;
 }
 
 TuringMachine::TuringMachine(std::vector<Card> cards_in) : 
 	cards(cards_in)
 {
 	InitializeMemory();
+	currentCard = 0;
 }
 
-TuringMachine::TuringMachine(const TuringMachine& other)
+TuringMachine::TuringMachine(const TuringMachine& other) :
+	cards(other.cards), memory(other.memory) 
 {
-	InitializeMemory();
-	cards = other.cards;
+	// TODO figure out how to copy the position iterator
+	currentCard = other.currentCard;
 }
 
 TuringMachine::TuringMachine(TuringMachine&& other)
@@ -23,8 +66,11 @@ TuringMachine::TuringMachine(TuringMachine&& other)
 	cards = std::move(other.cards);
 }
 
-void TuringMachine::InitializeMemory()
+/*
+ * Public Functions
+ */
+void TuringMachine::Tick()
 {
-	memory.push_back(false);
-	currentPos = memory.begin();
+	// Get the current card, and execute the appropriate instruction
+	ExecuteInstruction[cards[currentCard].instructions[*currentPos]];
 }
--- TuringMachine.hpp
+++ TuringMachine.hpp
@@ -14,15 +14,26 @@ private:
 	std::list<bool>				memory;
 
 	std::list<bool>::iterator	currentPos;
+	unsigned int				currentCard;
 
 	/*
 	 * Initializes the memory of the TuringMachine.
 	 */
 	void						InitializeMemory();
 
+	/*
+	 * Executes the given instruction, updating currentPos and currentCard
+	 */
+	void						ExecuteInstruction(const Instruction& instruction);
+
 public:
 								TuringMachine();
 								TuringMachine(std::vector<Card> cards);
 								TuringMachine(const TuringMachine& other);
 								TuringMachine(TuringMachine&& other);
+	
+	/*
+	 * Ticks the simulation forward by executing the next instruction
+	 */
+	void						Tick();
 };
